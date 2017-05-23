66b7616a98b388cdca2fe03b269328798e287676
UITEAK <as3427@naver.com>
2017-05-23 22:48:22 +0900

    UI_test update

---
+++ UI_test/app/src/main/java/com/example/ui_test/School_Serch.java
@@ -0,0 +1,15 @@
+package com.example.ui_test;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+/**
+ * Created by Administrator on 2017-05-20.
+ */
+
+public class School_Serch extends ActionBarActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.school_serch);
+    }
+}
